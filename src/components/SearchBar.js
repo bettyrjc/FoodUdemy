1e029ca9dad8867414bb42c203c32e14020e9408
Betty <bettyjimenez3010@gmail.com>
2021-01-12 16:03:39 -0400

    [ADD] api added

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,25 +1,43 @@
 import React from 'react'
 import { View,  StyleSheet, TextInput } from 'react-native'
 import { Ionicons } from '@expo/vector-icons'; 
-const SearchBar = () => {
+
+const SearchBar = ({term, onTermChange, onTermSubmit}) => {
     return (
-        <View style={styles.background}>
-            <Ionicons name='search' size={30}/>
+        <View style={styles.backgroundStyle}>
+            <Ionicons name='search' style={ styles.iconStyle}/>
             <TextInput
+                autoCapitalize='none'
+                autoCorrect={false}
+                value={term}
+                onChangeText={onTermChange}
+                style={styles.inputStyle}
                 placeholder='Search...'
+                onEndEditing={onTermSubmit}
+                
             />
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    background: {
+    backgroundStyle: {
         backgroundColor: '#F0EEEE',
+        marginTop: 10,
         height: 50,
         borderRadius: 5,
         marginHorizontal: 15,
         display: 'flex',
-        
+        flexDirection: 'row',
+        alignItems: 'center'
     },
+    inputStyle: {
+        flex: 1,
+        fontSize: 18
+    },
+    iconStyle: {
+        fontSize: 35,
+        alignSelf: 'center'
+    }
 })
 export default SearchBar
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,16 +1,47 @@
-import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import React, {useState} from 'react'
+import { View,  StyleSheet, Text } from 'react-native'
 import SearchBar from '../components/SearchBar'
-
+import yelp from '../api/yelp'
 const SearchScreen = () => {
+
+    const [term, setTerm] = useState('')
+    const [results, setResults] = useState([])
+    const [errorMessage, setErrorMessage] = useState('')
+    const searchApi = async (searchTerm) => {
+        console.log('hi there!');
+        try {
+            const response = await yelp.get('/search', {
+            params: {
+                limit: 50,
+                term: searchTerm,
+                location: 'san jose',
+            },
+        });
+        setResults(response.data.businesses)
+        } catch (err) {
+            setErrorMessage('Something went wrog')
+            console.log('err', err);
+        }
+    }
+
+    // call search api
+    // is first render
+    // searchApi('pasta') BAD CODE
     return (
         <View>
-            <SearchBar/>
+            <SearchBar
+                onTermSubmit={()=>searchApi(term)}
+                term={term}
+                onTermChange={setTerm}
+            />
+            {
+                errorMessage
+                    ? <Text>{errorMessage}</Text>
+                    : <Text>{results.length}</Text>
+            }
         </View>
     )
 }
 
-const styles = StyleSheet.create({
-    
-})
+const styles = StyleSheet.create({})
 export default SearchScreen
